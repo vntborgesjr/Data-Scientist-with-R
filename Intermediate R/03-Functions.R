c11deccd2b43443ec50a2cd26e8733af0d2174b9
Vitor Borges Jr <Vitor>
2020-09-16 20:40:13 -0300

    Update 03-Functions.R

--- Intermediate R/03-Functions.R
+++ Intermediate R/03-Functions.R
@@ -51,3 +51,123 @@ mean(linkedin, na.rm = TRUE)
 mean(abs(linkedin - facebook), na.rm = TRUE)
 
 # Writing Functions ------------------------------------------------
+# Create a function pow_two()
+pow_two <- function(x) {
+  y <- x^2
+  return(y)
+}
+
+# Use the function
+pow_two(12)
+
+# Create a function sum_abs()
+sum_abs <- function(a, b) {
+  a <- abs(a)
+  b <- abs(b)
+  c <- sum(a, b)
+  return(c)
+}
+
+# Use the function
+sum_abs(-2, 3)
+
+# Define the function hello()
+hello <- function() {
+  print("Hi there!")
+  return(TRUE)
+}
+
+# Call the function hello()
+hello()
+
+# Finish the pow_two() function
+# Add an optional argument, named print_info, that is TRUE by default.
+# Wrap an if construct around the print() function: this function should only be 
+# executed if print_info is TRUE.
+pow_two <- function(x, print_info = TRUE) {
+  y <- x ^ 2
+  if (print_info == TRUE) {
+    print(paste(x, "to the power two equals", y))
+  }
+  return(y)
+}
+
+# Define the interpret function
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return(num_views)
+  } else {
+    print("Try to be more visible!")
+    return(0)
+  }
+}
+
+# Call the interpret function twice
+interpret(linkedin[1])
+interpret(facebook[2])
+
+# The interpret() can be used inside interpret_all()
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return(num_views)
+  } else {
+    print("Try to be more visible!")
+    return(0)
+  }
+}
+
+# Define the interpret_all() function
+# views: vector with data to interpret
+# return_sum: return total number of views on popular days?
+interpret_all <- function(views, return_sum = TRUE) {
+  count <- 0
+  
+  for (v in views) {
+    count <- count + interpret(v)
+  }
+  
+  if (return_sum == TRUE) {
+    return(count)
+  } else {
+    return(NULL)
+  }
+}
+
+# Call the interpret_all() function on both linkedin and facebook
+interpret_all(linkedin, return_sum = TRUE)
+
+# R Packages ------------------------------------------------
+
+# Install packages
+install.packages("ggvis")
+
+# Check out the currently attached packages 
+search() # search list of R workspace
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Retry the qplot() function
+qplot(mtcars$wt, mtcars$hp)
+
+# Check out the currently attached packages again
+search()
+
+# Different ways to load a package
+
+# Chunk 1
+library(data.table)
+require(rjson)
+
+# Chunk 2
+library("data.table")
+require(rjson)
+
+# Chunk 3
+library(data.table)
+require(rjson, character.only = TRUE)
+
+# Chunk 4
+library(c("data.table", "rjson"))

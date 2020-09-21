2fce25da34a963e0a2fe60b8f184eddd065671f6
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-09-21 12:22:57 -0300

    Create 03-Selecting-and-Transforming-Data.R

---
+++ Data-Manipulation-with-dplyr/03-Selecting-and-Transforming-Data.R
@@ -0,0 +1,6 @@
+# --------------------------------------------------- 
+# Data Manipulation with dplyr - Selecting and Transforming Data 
+# 20 set 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+#

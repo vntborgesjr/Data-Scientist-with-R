1e9bd89df57f8fa785abea1a148b9d3852f9be39
Vitor Borges Jr <Vitor>
2020-12-30 09:34:25 -0300

    Update 04-Case-Study-National-Occupational-mean-wage.R

--- Cluster-Analysis-in-R/04-Case-Study-National-Occupational-mean-wage.R
+++ Cluster-Analysis-in-R/04-Case-Study-National-Occupational-mean-wage.R
@@ -32,6 +32,7 @@ plot(dend_colored)
 # Hierarchical clustering: Preparing for exploration
 library(tibble)
 library(tidyr)
+library(ggplot2)
 
 # Use rownames_to_column to move the rownames into a column of the data frame
 df_oes <- rownames_to_column(as.data.frame(oes), var = 'occupation')
@@ -57,3 +58,37 @@ ggplot(gathered_oes, aes(x = year, y = mean_salary, color = factor(cluster))) +
   geom_line(aes(group = occupation))
 
 # Reviewing the HC results ------------------------------------------------
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = oes, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)
+
+# K-means: Average Silhouete Widths
+# Use map_dbl to run many models with varying value of k
+sil_width <- map_dbl(2:10,  function(k){
+  model <- pam(oes, k = k)
+  model$silinfo$avg.width
+})
+
+# Generate a data frame containing both k and sil_width
+sil_df <- data.frame(
+  k = 2:10,
+  sil_width = sil_width
+)
+
+# Plot the relationship between k and sil_width
+ggplot(sil_df, aes(x = k, y = sil_width)) +
+  geom_line() +
+  scale_x_continuous(breaks = 2:10)
